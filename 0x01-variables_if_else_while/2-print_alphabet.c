20ebb76c481179669b615201800d2447fc178da2
hbankole <damilarehammed.hd@gmail.com>
2022-10-16 03:01:01 -0700

    I sometimes suffer from insomnia

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ *main - I sometimes suffer from insomnia.
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	for (char c = 'a'; c <= 'z'; ++c)
+		putchar(c);
+	putchar("\n");
+	return (0);
+}
