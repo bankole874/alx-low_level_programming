1a433228709ea2cc4cf8ff410514c1f47f891374
hbankole <damilarehammed.hd@gmail.com>
2022-10-16 04:53:20 -0700

    When I was having that alphabet soup,...

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ *main - print alphabt
+ *
+ *return: Always 0 (Success)
+ **/
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c != 'e' && c != 'q'; c++)
+		{
+			putchar(c)
+		}
+	}
+	putchar('\n');
+	return (0);
+}
