37b4e5f3eee1062a72b9bb4fcbed88bc7b41c4e6
hbankole <damilarehammed.hd@gmail.com>
2022-10-16 05:42:21 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Numberz
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int c;
+
+	for (c = 48; c < 58; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
