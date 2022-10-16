603d1576cc449c98e8b7b4d0e61c9f89d0d8b61f
hbankole <damilarehammed.hd@gmail.com>
2022-10-16 14:29:36 -0700

    patience, persistence and perspiration

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print combination
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int c;
+
+	for (c = 48; c < 57; c++)
+	{
+		putchar(c);
+		putchar(',');
+	}
+	putchar(57);
+	return (0);
+}
