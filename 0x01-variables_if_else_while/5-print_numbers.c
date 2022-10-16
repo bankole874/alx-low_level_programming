b35f7bf4e3969a95145451025f95daf93d080757
hbankole <damilarehammed.hd@gmail.com>
2022-10-16 05:17:53 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Numbers
+ *
+ * return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int c;
+
+	for (c = 0; c <= 10; c++;)
+	{
+		printf("%d", c);
+	}
+	printf("\n");
+	return (0);
+}
