09d92baf4995d9e43cccc117e5726f85b675b1c3
hbankole <damilarehammed.hd@gmail.com>
2022-10-17 10:29:43 -0700

    UNIX

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h> 
+#include <stdlib.h>
+
+/**
+ * main - UNIX
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	write(1, "and that piece of art is useful\" Dora Kopar, 2015-10-19\n", 59);
+	return (1);
+}
