8704cc3b63ab77ed4d88939ad2b5490321714689
hbankole <damilarehammed.hd@gmail.com>
2022-10-18 10:06:42 -0700

    Software is eating the world

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+/**
+ * main - print_comb5
+ *
+ *  Return: Always 0 (Success)
+ */
+
+int main()
+{
+	int a;
+	int b;
+	int c;
+	int d;
+
+	for (a = 48; a < 58; a++)
+	{
+		for (b = 48; b < 58; b++)
+		{
+			for (c = 48; c < 58; c++)
+			{
+				for (d = 48; d < 58; d++)
+				{
+					if (a + b < c + d && a <= c && b <= d)
+					{
+						putchar(a);
+						putchar(b);
+						putchar(' ');
+						putchar(c);
+						putchar(d);
+						if (a == 57 && b == 56 && c == 57 && d == 57)
+						{
+						break;
+						}
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	return(0);
+}
+
