d64e7569746ec90618885d4167e0ffe58f4e3eb3
hbankole <damilarehammed.hd@gmail.com>
2022-10-19 11:23:27 -0700

    10 X alphabets

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * 10 x alphabet
+ */
+
+void print_alphabet_x10(void)
+{
+	int a;
+	int b;
+
+	do
+	{
+		for (a = 97; a < 123; a++)
+		{
+			_putchar(a);
+		}
+		_putchar('\n');
+		b++;
+	}
+	while (b < 10);
+
+	return (0);
+}
+	
