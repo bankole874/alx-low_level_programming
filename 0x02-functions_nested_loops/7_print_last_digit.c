825e260f80136f334fd676f1f09ab7af7f89669f
hbankole <damilarehammed.hd@gmail.com>
2022-10-20 06:51:41 -0700

    print_last_digit

---
+++ 0x02-functions_nested_loops/7_print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit of a number
+ *
+ * @l: last digit
+ * Return: Always 0 (Success)
+ */
+
+int print_last_digit(int l)
+{
+	if (l > 0)
+	{
+		_putchar(l + 48);
+	}
+	else if  (l < 0)
+	{
+		_putchar(-l + 48);
+	}
+}
