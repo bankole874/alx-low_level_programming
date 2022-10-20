d14c259c7b478f7b036e157e0b3476a904b6055a
hbankole <damilarehammed.hd@gmail.com>
2022-10-20 03:46:00 -0700

    Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of a number.
+ *
+ * @n: number to check its sign
+ *
+ * Return: Always 0 (Success)
+ */
+
+int print_sign(int n)
+{
+	if (c < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else if (c == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+		_putchar('+');
+		return (1);
+	}
+}
