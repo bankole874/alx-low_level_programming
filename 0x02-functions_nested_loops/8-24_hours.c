4c4de6ead33f607d8e1c98d61311884ea10dead1
hbankole <damilarehammed.hd@gmail.com>
2022-10-20 10:42:35 -0700

    Jack Bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ */
+
+void jack_bauer(void)
+{
+	int a;
+	int b;
+	int c;
+	int d;
+
+	for (a = 48; a < 58; a++)
+	{
+		for (b = 48; b < 58; b++)
+		{
+			for (c = 48; c < 58; c++)
+			{
+				for (d = 48; d < 58; d++)
+				{
+					if (a < 51 && b < 52 && c < 54 && d < 58)
+					{
+					_putchar(a);
+					_putchar(b);
+					_putchar(':');
+					_putchar(c);
+					_putchar(d);
+					if (a == 50 && b == 51 && c == 53 && d == 57)
+					{
+						break;
+					}
+					putchar(',');
+					putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
