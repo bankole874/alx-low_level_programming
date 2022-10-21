92489eaeac62b2c1226e45a67e008fab4600390f
hbankole <damilarehammed.hd@gmail.com>
2022-10-21 13:30:30 -0700

    9times

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * times_table - multiplication table 9
+ *
+ */
+
+void times_table(void)
+{
+	int a, b, c;
+
+	for (a = 0; a <= 9; a++)
+	{
+		_putchar('0');
+		for (b = 0; b <= 9; b++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			c = a * b;
+
+			if (c <= 9)
+			{
+				_putchar(' ');
+			} else
+				_putchar('0' + (c / 10));
+				_putchar('0' + (c % 10));
+		}
+		_putchar('\n');
+	}
+}
