0b38bc156ef34951380839ba8430b0970f920e84
hbankole <damilarehammed.hd@gmail.com>
2022-10-24 14:03:05 -0700

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the value of two integers
+ * @a: input a
+ * @b: input b
+ * Return: 0 Always(Success)
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = &b;
+	*b = &a;
+}
