ca447c0f50b667ddcca118ed8f2dbd07233813d5
hbankole <damilarehammed.hd@gmail.com>
2022-10-25 09:48:52 -0700

    reverse strings

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,10 +10,15 @@ void rev_string(char *s)
 {
 	int count = 0;
 	int i;
+	int j;
 
 	while (s[count] != '\0')
 	{
 		count++;
+	}
+	for (i = j; j <= count; j++)
+	{
+		_putchar(s[j]);
 	}
 	for (i = 1; i <= count; i++)
 	{
