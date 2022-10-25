bdd2e6f3436c0424a726e1350d1490f5977b3f39
hbankole <damilarehammed.hd@gmail.com>
2022-10-25 01:31:12 -0700

    reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string.
+ * @s: input character
+ * Return: no
+ */
+
+void rev_string(char *s)
+{
+	int count = 0;
+	int i = 1;
+
+	while (s[count] != '\0')
+	{
+		count++;
+	}
+	for (i = 1; i <= count; i++)
+	{
+		_putchar(s[count - i]);
+	}
+	putchar('\n');
+}
