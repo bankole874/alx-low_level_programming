cda886f33afe61309fb2cb0bb2b71a130b32a481
hbankole <damilarehammed.hd@gmail.com>
2022-10-30 03:28:52 -0700

    keygen

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,29 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ *
+ *
+ */
+
+int main(void)
+{
+	int random = 0, count = 0;
+
+	srand(time(NULL));
+	while (c < 2772)
+	{
+		r = rand % 128;
+		if ((count + random) > 2772)
+		{
+			break;
+		}
+		count = count + random;
+		printf("%c", random);
+	}
+	printf("%c\n", (2772 - c));
+
+	return (0);
+}			
