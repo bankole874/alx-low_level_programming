92cf52fbfa57ae8d60cb0f1bafc4653c7e2c92b3
hbankole <damilarehammed.hd@gmail.com>
2022-11-14 13:55:21 -0800

    bingo

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - describes a dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: dog owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+/**
+ * dog_t-Typedef for struct dog
+ */
+typedef struct dog dog_t;
