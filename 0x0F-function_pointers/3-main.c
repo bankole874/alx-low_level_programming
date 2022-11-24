917c354121e5c21e66729c40018fa97b7c424a2d
hbankole <damilarehammed.hd@gmail.com>
2022-11-23 22:21:35 -0800

    op functions

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,34 @@
+#include "3-calc.h"
+
+/**
+ * main - contain main function only.
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: Always(0) Success
+ */
+
+int main(int argc, char *argv[])
+{
+	int a, b;
+	int (*operation)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	if (argv[2][1])
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	operation = get_op_func(argv[2]);
+
+	if (operation == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+}
