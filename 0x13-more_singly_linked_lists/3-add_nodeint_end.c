1480a187c31d83aabe67a1ff7b2d1fff0afd4b29
hbankole <damilarehammed.hd@gmail.com>
2022-11-29 09:32:44 -0800

    more linked lists

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * add_nodeint_end - function pointer
+ * @head: a pointer to a pointer.
+ * @n: node that store data.
+ * Return: new element.
+ */
+
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *endnode;
+	listint_ *newnode;
+
+	newnode = malloc(sizeof(listint_t));
+
+	if (!newnode)
+	{
+		return (NULL);
+	}
+
+	newnode-> = n;
+	newnode->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = newnode;
+		return (newnode);
+	}
+	endnode = *head;
+
+	while (endnode->next != NULL)
+	{
+		endnode = endnode->next;
+	}
+	endnode->next = newnode;
+
+	return (newnode);
+}
