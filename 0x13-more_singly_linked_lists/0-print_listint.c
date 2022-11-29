efd3b12e0390ee54fcb7cc16c078241a0177d230
hbankole <damilarehammed.hd@gmail.com>
2022-11-29 04:48:59 -0800

    more singly linked lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -20,17 +20,16 @@ size_t print_listint(const listint_t *h)
 	return (i);
 }
 
-/**
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
 #include <lists.h>
 
-
+/**
  *
  * main - check the code
  * Return: Always 0.
-
+*/
 
 int main(void)
 {
@@ -45,4 +44,14 @@ int main(void)
 
 	if (new == NULL)
 	{
-*/
+		printf("Error\n");
+		return (1);
+	}
+	new->n = 9;
+	new->next = head;
+	head = new;
+	n = print_listint(head);
+	printf("-> %lu elements\n", n);
+	free(new);
+	return (0);
+}
