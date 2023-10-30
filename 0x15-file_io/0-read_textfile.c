21f8e6a60e95ad5a1f690bd8fee3a91b77ef099e
bankole874 <damilarehammed.hd@gmail.com>
2023-10-30 15:56:53 -0700

    added 0x15

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+ssize_t read_textfile(const char *filename, size_t letters) {
+	FILE *hfp;
+	char *hbuffer;
+	size_t hbytes_read;
+
+	if (filename == NULL) {
+		return 0;
+	}
+
+	hfp = fopen(filename, "r");
+	
+	if (hfp == NULL) {
+		return 0;
+	}
+	
+	hbuffer = malloc(letters + 1);
+	if (hbuffer == NULL) {
+		fclose(hfp);
+		return 0;
+	}
+
+	hbytes_read = fread(hbuffer, 1, letters, hfp);
+	
+	if (hbytes_read != letters) {
+		fclose(hfp);
+		return 0;
+	}
+
+	fputs(hbuffer, stdout);
+
+	fclose(hfp);
+
+	return(hbytes_read);
+}
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdbool.h>
+#include <sys/wait.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+
+
+#endif /* MAIN_H */
