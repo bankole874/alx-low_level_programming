23460a5b224bbaae24e3e6e51efb460a9dd522b7
bankole874 <damilarehammed.hd@gmail.com>
2023-10-31 16:46:41 -0700

    added task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -7,30 +7,31 @@
 #define BUFFER_SIZE 1024
 
 /**
- * copy_to_file - a function.
- * @file_from: file.
- * @file_to: file.
+ * main - a function.
+ * @ac: argument.
+ * @av: argument.
  * Return: 0
  */
 
-int main(int ac, char **av) {
-
+int main(int ac, char **av)
+{
 	int hfd_from;
 	int hfd_to;
 	ssize_t hwrite_bytes;
 	ssize_t hread_bytes;
 	char hbuffer[BUFFER_SIZE];
 
-	if (ac != 3) {
+	if (ac != 3)
+	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		return 97;
+		exit(97);
 	}
 	hfd_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC);
 	if (hfd_to == -1)
-        {
+	{
 		dprintf(2, "Error: Can't write to %s\n", av[2]);
 		exit(99);
-        }
+	}
 	hfd_from = open(av[1], O_RDONLY);
 	if (hfd_from == -1)
 	{
@@ -43,7 +44,7 @@ int main(int ac, char **av) {
 		hwrite_bytes = write(hfd_to, hbuffer, hread_bytes);
 		if (hwrite_bytes != hread_bytes)
 		{
-			dprintf(2, "Error: Can't close fd %s\n", av[2] );
+			dprintf(2, "Error: Can't close fd %s\n", av[2]);
 			exit(100);
 		}
 
