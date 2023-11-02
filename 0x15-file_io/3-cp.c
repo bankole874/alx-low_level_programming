61e0c7f93f8feead5806bce315bc93b8b3190cc2
bankole874 <damilarehammed.hd@gmail.com>
2023-11-02 04:40:09 -0700

    updated task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -39,24 +39,27 @@ int main(int ac, char **av)
 		exit(98);
 	}
 	hread_bytes = read(hfd_from, hbuffer, BUFFER_SIZE);
-	while ((hread_bytes) != -1)
+	while ((hread_bytes) > 0)
 	{
 		hwrite_bytes = write(hfd_to, hbuffer, hread_bytes);
-		if (hwrite_bytes < hread_bytes)
+		if (hwrite_bytes > hread_bytes)
 		{
-			dprintf(2, "Error: Can't write fd %s\n", av[2]);
+			dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", av[2]);
 			exit(99);
 		}
-	}
-	if (close(hfd_from) == -1)
-	{
-		dprintf(2, "Error: Can't close fd %d\n", hfd_from);
-		       	exit(100);
-	}
-	if (close(hfd_to) == -1)
-	{
-		dprintf(2, "Error: Can't close fd %d\n", hfd_to);
-		exit(100);
+		if (close(hfd_from) == -1)
+		{
+			dprintf(2, "Error: Can't close fd %d\n", hfd_from);
+			exit(100);
+		}
+		if (close(hfd_to) == -1)
+		{
+			dprintf(2, "Error: Can't close fd %d\n", hfd_from);
+			exit(100);
+		}
+		close(hfd_from);
+		close(hfd_to);
+		return (0);
 	}
 	return (0);
 }
