4e3452ab8f72e64ec043669893b782ca17a77065
bankole874 <damilarehammed.hd@gmail.com>
2023-11-02 05:22:28 -0700

    updated task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -15,28 +15,24 @@
 
 int main(int ac, char **av)
 {
-	int hfd_from;
-	int hfd_to;
+	int hfd_from, int hfd_to;
 	ssize_t hwrite_bytes;
 	ssize_t hread_bytes;
 	char hbuffer[BUFFER_SIZE];
 
 	if (ac != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	}
 	hfd_from = open(av[1], O_RDONLY);
 	if (hfd_from == -1)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", av[1]);
-		exit(98);
+		dprintf(2, "Error: Can't read from file %s\n", av[1]), exit(98);
 	}
 	hfd_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (hfd_to == -1)
 	{
-		dprintf(2, "Error: Can't write to %s\n", av[2]);
-		exit(99);
+		dprintf(2, "Error: Can't write to %s\n", av[2]), exit(99);
 	}
 	hread_bytes = read(hfd_from, hbuffer, BUFFER_SIZE);
 	while ((hread_bytes) > 0)
@@ -44,18 +40,15 @@ int main(int ac, char **av)
 		hwrite_bytes = write(hfd_to, hbuffer, hread_bytes);
 		if (hwrite_bytes > hread_bytes)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", av[2]);
-			exit(99);
+			dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", av[2]), exit(99);
 		}
 		if (close(hfd_from) == -1)
 		{
-			dprintf(2, "Error: Can't close fd %d\n", hfd_from);
-			exit(100);
+			dprintf(2, "Error: Can't close fd %d\n", hfd_from), exit(100);
 		}
 		if (close(hfd_to) == -1)
 		{
-			dprintf(2, "Error: Can't close fd %d\n", hfd_from);
-			exit(100);
+			dprintf(2, "Error: Can't close fd %d\n", hfd_from), exit(100);
 		}
 		close(hfd_from);
 		close(hfd_to);
