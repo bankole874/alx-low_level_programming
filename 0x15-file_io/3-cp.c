f0ff7990c8311c79659c0c3c599f1293e8ee4196
Hammed Bankole <40712924+bankole874@users.noreply.github.com>
2023-11-03 03:06:12 +0100

    Update 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -26,7 +26,7 @@ int main(int ac, char **av)
 	{
 		dprintf(2, "Error: Can't read from file %s\n", av[1]), exit(98);
 	}
-	hfd_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	hfd_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC | O_APPEND, 0664);
 	if (hfd_to == -1)
 	{
 		dprintf(2, "Error: Can't write to %s\n", av[2]), exit(99);
