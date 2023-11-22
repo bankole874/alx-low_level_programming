67d6272c40e465d30a33acbd80afec03057e486d
bankole874 <damilarehammed.hd@gmail.com>
2023-11-21 16:45:37 -0800

    added lists.h

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - prints list.
+ * @h: var h.
+ * Return: int.
+ */
+
+size_t print_list(const list_t *h)
+{
+int count;
+struct list_s *a;
+
+count = 0;
+
+if (h->str == NULL)
+{
+printf("[%d] ", 0);
+printf("(%s)\n", "nil");
+h = h->next;
+count++;
+}
+
+while (h != NULL)
+{
+printf("[%d] ", h->len);
+printf("%s\n", h->str);
+h = h->next;
+count++;
+}
+
+return (count);
+
+}
+
