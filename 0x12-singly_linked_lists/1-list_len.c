376ceaaa3ef5fe20f60b7be5598db7442bee9858
Hammed Bankole <40712924+bankole874@users.noreply.github.com>
2023-11-23 08:57:49 +0000

    updated lists.h & 1-list_len.c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * list_len - lists length.
+ * @h: var h.
+ * Return: int.
+ */
+
+size_t list_len(const list_t *h)
+{
+
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,10 @@
 #ifndef _LISTS_H_
 #define _LISTS_H_
 
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
