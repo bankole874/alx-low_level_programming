47a1f3e4cc2902798b9228101205c2d82350053d
Hammed Bankole <40712924+bankole874@users.noreply.github.com>
2023-11-23 09:38:19 +0000

    1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -12,10 +12,14 @@
 size_t list_len(const list_t *h)
 {
 int count = 0;
+if (h == NULL)
+{
+return (0);
+}
 while (h->next != NULL)
 {
 count++;
 h = h->next;
 }
-return count+1;
+return (count + 1);
 }
