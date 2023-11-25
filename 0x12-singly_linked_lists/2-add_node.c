8853a8476eb9ed46ce3836a874613b7deffbb1ec
Hammed Bankole <40712924+bankole874@users.noreply.github.com>
2023-11-25 16:57:26 +0000

    updated 2-main.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -9,23 +9,23 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
-    list_t *temp = malloc(sizeof(struct list_s));
-    int length= 0;
-    if (temp == NULL)
-    {
-        return (NULL);
-    }
+list_t *temp = malloc(sizeof(struct list_s));
 
-    while (str[length])
-    {
-        length++;
-    }
+int length = 0;
+if (temp == NULL)
+{
+return (NULL);
+}
+while (str[length])
+{
+length++;
+}
 
-    temp->str = strdup(str);
-    temp->len = length;
-    temp->next = *head;
+temp->str = strdup(str);
+temp->len = length;
+temp->next = *head;
 
-    *head = temp;
+*head = temp;
 
-    return (temp);
+return (temp);
 }
