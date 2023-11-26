0e89457aa60191f74b7b83a6c33d88b3c9671ad8
Hammed Bankole <40712924+bankole874@users.noreply.github.com>
2023-11-26 08:45:36 +0000

    updated 3-add_node_end.c & lists.h

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,2 +1,38 @@
 #include "lists.h"
 
+/**
+ * add_node_end - add end node.
+ * @head: var head.
+ * @str: variable str.
+ * Return: int and str.
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+list_t *temp = malloc(sizeof(list_t));
+int length = 0;
+
+while (str[length])
+{
+length++;
+}
+
+temp->str = strdup(str);
+temp->len = length;
+temp->next = NULL;
+
+if (*head == NULL)
+{
+*head = temp;
+return (temp);
+}
+
+list_t *current = *head;
+
+while (current->next != NULL)
+{
+current = current->next;
+}
+current->next = temp;
+return (temp);
+}
