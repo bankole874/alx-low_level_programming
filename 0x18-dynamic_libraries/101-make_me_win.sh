04961ad991fe0d545a39a15c777c1f522a9052ed
bankole874 <damilarehammed.hd.com>
2023-12-13 01:46:48 +0100

    added 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -O /tmp/win_num.so https://github.com/Zevrov/holbertonschool-low_level_programming/blob/master/0x18-dynamic_libraries/win.so
+wget -O /tmp/win_num.so https://github.com/bankole874/alx-low_level_programming/blob/master/0x18-dynamic_libraries/win_num.so
 export LD_PRELOAD=/tmp/win_num.so
--- 0x18-dynamic_libraries/win_num.c
+++ 0x18-dynamic_libraries/win_num.c
@@ -2,6 +2,7 @@
 
 int winning_numbers[] = {9, 8, 10, 24, 75, 9};
 
-int *get_winning_numbers() {
+int *get_winning_numbers()
+{
   return winning_numbers;
 }
